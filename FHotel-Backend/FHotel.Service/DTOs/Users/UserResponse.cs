292c8844fcd143ab68b463e6f64441029e21c41e
Nhat Pham <nhat2up@gmail.com>
2024-10-14 21:54:52 +0700

    remove hotelid

--- FHotel-Backend/FHotel.Service/DTOs/Users/UserResponse.cs
+++ FHotel-Backend/FHotel.Service/DTOs/Users/UserResponse.cs
@@ -20,7 +20,6 @@ namespace FHotel.Services.DTOs.Users
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
         public bool? Sex { get; set; }
-        public Guid? HotelId { get; set; }
         public Guid? RoleId { get; set; }
         public DateTime? CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
